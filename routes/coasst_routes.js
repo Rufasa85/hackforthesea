c064cf07ad734af471327c8660119a5561768168
Joe Rehfuss <joe.rehfuss@gmail.com>
2018-09-22 15:27:22 -0700

    cloudinary uploads

--- routes/coasst_routes.js
+++ routes/coasst_routes.js
@@ -1,4 +1,14 @@
 module.exports = function(app, db) {
+  const cloudinary = require('cloudinary');
+  const apiKeys = require('../apikeys.js');
+  const multer = require('multer');
+  const uploads = multer({dest:'../uploads'});
+
+  cloudinary.config({
+    cloud_name:apiKeys.cloudName,
+    api_key:apiKeys.apiKey,
+    api_secret:apiKeys.apiSecret
+  })
 
   app.get('/', function (req, res) {
     data = db.collection('metadata').find({}).toArray(function(err, result){
@@ -6,9 +16,14 @@ module.exports = function(app, db) {
       res.render('index');
     });
   })
-  app.post('/', function (req, res) {
-    db.collection('metadata').insert(req.body, (err, results) => {});
-    res.redirect('/');
+  app.post('/', uploads.single('imageUpload'),
+  function (req, res) {
+    cloudinary.uploader.upload(req.file.path, function (result) {
+      console.log(result.url);
+      res.send(result.url);
+      // db.collection('metadata').insert(req.body, (err, results) => {});
+      // res.redirect('/');
+    })
   })
   app.get('/new', function (req, res) {
     res.render('new');
