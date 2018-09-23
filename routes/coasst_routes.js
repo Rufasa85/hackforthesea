d6a3261a8c674f6fd8345e809b2d729bae29fe0e
Joe Rehfuss <joe.rehfuss@gmail.com>
2018-09-22 17:55:22 -0700

    deploying

--- routes/coasst_routes.js
+++ routes/coasst_routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app, db) {
   const cloudinary = require('cloudinary');
   const multer = require('multer');
-  const uploads = multer({dest:'../uploads'});
+  const uploads = multer({dest:'/uploads'});
   require('dotenv').config();
 
   cloudinary.config({
